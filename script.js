52a957cd1ba43783f3e975ed1e7abad4f5e68b7a
EmreDogu <emredogu28@gmail.com>
2022-03-25 16:56:43 +0300

    Adding roundPlay function which checks who has won round

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ function computerPlay() {
     let random = array[Math.floor(Math.random()*array.length)];
     return random;
 }
+
+function roundPlay(computerSelection, playerSelection) {
+    if (playerSelection==computerSelection) {
+        alert("It's a tie!");
+    }else if (playerSelection=="Rock" && computerSelection=="Scissors") {
+        alert("You win! Rock beats Scissors");
+    }else if (playerSelection=="Paper" && computerSelection=="Rock") {
+        alert("You win! Paper beats Rock");
+    }else if (playerSelection=="Scissors" && computerSelection=="Paper") {
+        alert("You win! Scissors beats Paper");
+    }else {
+        alert("You lose! " + computerSelection + " beats " + playerSelection);
+    }
+}
+
+var computerSelection = computerPlay();
+var playerSelection = "Rock";
+
+console.log(roundPlay(computerSelection, playerSelection));
