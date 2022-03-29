befec145644d23adde446633e0dc81e7422b4c9b
EmreDogu <emredogu28@gmail.com>
2022-03-29 21:58:14 +0300

    Functionally done probably clean it up later

--- script.js
+++ script.js
@@ -1,51 +1,102 @@
+//css ve genel olarak toparlama var (query'leri playerwins gibi başa alma vs.)
+
+let playerWins = 0;
+let computerWins = 0;
+let gameEnd = 0;
+
 function computerPlay() {
     let array = ["rock", "paper", "scissors"];
-    let random = array[Math.floor(Math.random()*array.length)];
+    let random = array[Math.floor(Math.random() * array.length)];
     return random;
 }
 
 function roundPlay(computerSelection, playerSelection) {
-    if (!(playerSelection=="rock") && !(playerSelection=="paper") && !(playerSelection=="scissors")) {
-        return "wrong input";
-    }
-    else if (playerSelection==computerSelection) {
+    if (playerSelection == computerSelection) {
         return "tie";
-    }else if (playerSelection=="rock" && computerSelection=="scissors") {
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
         return true;
-    }else if (playerSelection=="paper" && computerSelection=="rock") {
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
         return true;
-    }else if (playerSelection=="scissors" && computerSelection=="paper") {
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
         return true;
-    }else {
+    } else {
         return false;
     }
 }
 
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    while (playerWins<3 && computerWins<3) {
-        let computerSelection = computerPlay();
-        let playerSelection = window.prompt("Enter either rock, paper or scissors:");
-        let result = roundPlay(computerSelection, playerSelection.toLowerCase());
-        if (result == "tie") {
-            alert("It's a tie!");
-        }else if (result == true) {
-            playerWins += 1;
-            alert("You win! " + playerSelection + " beats " + computerSelection.toLowerCase());
-        }else if (result == false) {
-            computerWins += 1;
-            alert("You lose! " + computerSelection + " beats " + playerSelection.toLowerCase());
-        }else {
-            alert("Please enter either rock, paper or scissors instead of random stuff");
-        }
+function checkGameEnd(computerWins, playerWins) {
+    const winCondition = document.querySelector(".winCondition");
+    if (computerWins > playerWins && computerWins == 3) {
+        winCondition.textContent = "Computer has won the game!";
+        return true;
+    } else if (playerWins > computerWins && playerWins == 3) {
+        winCondition.textContent = "Player has won the game!";
+        return true;
     }
-
-    if (computerWins > playerWins) {
-        alert("Computer has won the game!");
-    }else {
-        alert("Player has won the game!");
-    }
+    return false;
 }
 
-game();
+function restartGame() {
+    playerWins = 0;
+    computerWins = 0;
+    gameEnd = 0;
+    let situation = document.querySelector(".situation");
+    let winCondition = document.querySelector(".winCondition");
+    let playerWinCount = document.querySelector(".playerWinCount");
+    let computerWinCount = document.querySelector(".computerWinCount");
+    let restartButton = document.querySelector(".restart");
+    restartButton.remove();
+    situation.textContent = "Choose either rock, paper or scissors by clicking the corresponding image!";
+    winCondition.textContent = "First to 3 wins!";
+    playerWinCount.textContent = "?";
+    computerWinCount.textContent = "?";
+}
+
+const buttonList = document.querySelectorAll("button");
+buttonList.forEach(button => {
+    button.addEventListener("click", function () {
+        if (gameEnd) {
+            return;
+        }
+        let computerSelection = computerPlay();
+        const bool = roundPlay(computerSelection, button.className);
+        if (bool == true) {
+            playerWins++;
+            let situation = document.querySelector(".situation");
+            let winCondition = document.querySelector(".winCondition");
+            let playerWinCount = document.querySelector(".playerWinCount");
+            situation.textContent = "You win!";
+            winCondition.textContent = `${button.className} beats ${computerSelection}`;
+            playerWinCount.textContent = playerWins;
+            gameEnd = checkGameEnd(computerWins, playerWins);
+        } else if (bool == false) {
+            computerWins++;
+            let situation = document.querySelector(".situation");
+            let winCondition = document.querySelector(".winCondition");
+            let computerWinCount = document.querySelector(".computerWinCount");
+            situation.textContent = "You lose!";
+            winCondition.textContent = `${computerSelection} beats ${button.className}`;
+            computerWinCount.textContent = computerWins;
+            gameEnd = checkGameEnd(computerWins, playerWins);
+        } else {
+            let situation = document.querySelector(".situation");
+            let winCondition = document.querySelector(".winCondition");
+            situation.textContent = "It's a tie!";
+            winCondition.textContent = `Computer and you both pick ${computerSelection}`;
+        }
+
+        if (gameEnd === true) {
+            const announce = document.querySelector(".announce");
+            const button = document.createElement("button");
+            button.textContent = "Restart the Game!";
+            button.classList.add("restart");
+            announce.appendChild(button);
+        }
+    });
+});
+
+document.body.addEventListener("click", function (e) {
+    if (e.target.className == "restart") {
+        restartGame();
+    }
+});
